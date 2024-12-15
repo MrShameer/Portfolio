5fb4effac0d0c72a4838122f64f0469b1d3dd113
Abdul Rahman <67554400+MrShameer@users.noreply.github.com>
2024-12-15 12:44:15 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,6 @@ const repoName = "Portfolio"; // Replace with your GitHub repository name
 
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  assetPrefix: isProd ? `/${repoName}/` : "", // Prefix all assets with the repo name for GitHub Pages
-  basePath: isProd ? `/${repoName}` : "", // Add the repo name as the base path
-  trailingSlash: true, // Ensure trailing slashes for static export compatibility
-  output: "export", // Enable static export
 };
 
 export default nextConfig;
